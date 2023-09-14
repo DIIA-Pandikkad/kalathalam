cf7b89c150876b8c89f7ac43b2b5a94f872d4481
Thoufeeque Rahman <78486724+Thoufeeque-Rahman@users.noreply.github.com>
2023-09-14 11:37:41 +0000

    nav dash

--- routes/public.js
+++ routes/public.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('users/public/landing_page', { title: 'Public', home:true });
+  res.render('users/public/landing_page', { title: 'Public', dashboard:false });
 });
 
 router.get('/dashboard', function(req, res, next) {
-  res.render('users/public/dashboard', { title: 'About', home:false });
+  res.render('users/public/dashboard', { title: 'About', dashboard:true });
+});
+
+router.get('/demo', function(req, res, next) {
+  res.render('users/public/demo', { title: 'About', dashboard:true });
 });
 module.exports = router;
