d61c318d34d20393373163433e02f9b4c257fe98
Thoufeeque Rahman <78486724+Thoufeeque-Rahman@users.noreply.github.com>
2023-09-19 11:09:36 +0000

    result page  updates

--- routes/public.js
+++ routes/public.js
@@ -2,27 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('users/public/landing_page', { title: 'Public', dashboard:false });
+router.get('/', function (req, res, next) {
+  res.render('users/public/landing_page', { title: 'Public', dashboard: false });
 });
 
-router.get('/dashboard', function(req, res, next) {
-  res.render('users/public/dashboard', {  dashboard:true });
+router.get('/dashboard', function (req, res, next) {
+  res.render('users/public/dashboard', { dashboard: true });
 });
 
-router.get('/dashboard/results', function(req, res, next) {
+router.get('/dashboard/results', function (req, res, next) {
   var results = [
-    {CCode: 84, CName: 'John', Program: 'Song Arab',PCode: 'SS10', pos: 1, grade: 'A', score: 10},
-    {CCode: 84, CName: 'John', Program: 'Song Arab',PCode: 'SS10', pos: 2, grade: 'A', score: 9},
-    {CCode: 84, CName: 'John', Program: 'Song Arab',PCode: 'SS10', pos: 3, grade: 'A', score: 8},
-    {CCode: 84, CName: 'John', Program: 'Song Arab',PCode: 'SS10', pos: 4, grade: 'A', score: 7},
-    {CCode: 84, CName: 'John', Program: 'Song Arab',PCode: 'SS10', pos: 5, grade: 'A', score: 6},
-    {CCode: 84, CName: 'John', Program: 'Song Arab',PCode: 'SS10', pos: 6, grade: 'A', score: 5},
-  ];
-  res.render('users/public/results', {  dashboard:true,  results: results });
+    {
+      ProgramN: 'Song Arab',
+      ProgramC: 'SS10',
+      data: [{ CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 1, grade: 'A', score: 10 },
+      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 2, grade: 'A', score: 9 },
+      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 3, grade: 'A', score: 8 },
+      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 4, grade: 'A', score: 7 },
+      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 5, grade: 'A', score: 6 },
+      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 6, grade: 'A', score: 5 }]
+    }]
+
+    
+    
+  console.log(results[0]);
+  res.render('users/public/results', { dashboard: true, results: results[0] });
 });
 
-router.get('/demo', function(req, res, next) {
-  res.render('users/public/demo', { title: 'About', dashboard:true });
+
+router.get('/demo', function (req, res, next) {
+  res.render('users/public/demo', { title: 'About', dashboard: true });
 });
 module.exports = router;
