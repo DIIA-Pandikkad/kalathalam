2d5e6f790a0f30226e011353a5064b40b260322b
Thoufeeque Rahman <78486724+Thoufeeque-Rahman@users.noreply.github.com>
2023-10-03 09:34:24 +0000

    updates

--- routes/public.js
+++ routes/public.js
@@ -17,12 +17,41 @@ router.get('/dashboard/results', function (req, res, next) {
     {
       ProgramN: 'Song Arab',
       ProgramC: 'SS10',
-      data: [{ CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 1, grade: 'A', score: 10, first: true },
-      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 2, grade: 'A', score: 9, second: true },
-      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 3, grade: 'A', score: 8, third: true },
-      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 4, grade: 'A', score: 7,  },
-      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 5, grade: 'A', score: 6,  },
-      { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 6, grade: 'A', score: 5,  }]
+      data: [
+        { 
+          imageAdd:'https://i.ibb.co/cvwkGWy/photo-2022-09-11-15-08-59.jpg',
+          CCode: 84, 
+          CName: 'John', 
+          Program: 'Song Arab', 
+          PCode: 'SS10', 
+          pos: 1, 
+          grade: 'A', 
+          score: 10, 
+          first: true 
+        },
+        { 
+          imageAdd:'https://i.ibb.co/cvwkGWy/photo-2022-09-11-15-08-59.jpg',
+          CCode: 84, 
+          CName: 'John', 
+          Program: 'Song Arab', 
+          PCode: 'SS10', pos: 2, 
+          grade: 'A', score: 9, 
+          second: true 
+        },
+        { 
+          imageAdd:'https://i.ibb.co/cvwkGWy/photo-2022-09-11-15-08-59.jpg',
+          CCode: 84, 
+          CName: 'John', 
+          Program: 'Song Arab', 
+          PCode: 'SS10', 
+          pos: 3, 
+          grade: 'A', 
+          score: 8, 
+          third: true 
+        },
+        { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 4, grade: 'A', score: 7, },
+        { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 5, grade: 'A', score: 6, },
+        { CCode: 84, CName: 'John', Program: 'Song Arab', PCode: 'SS10', pos: 6, grade: 'A', score: 5, }]
     }]
 
     
