a03cf68cf3bec9869fd4fdfbabcf9822fd05c230
Thoufeeque Rahman <78486724+Thoufeeque-Rahman@users.noreply.github.com>
2023-10-20 20:25:42 +0530

    without await 2

--- helpers/userhelper.js
+++ helpers/userhelper.js
@@ -12,10 +12,10 @@ module.exports = {
     },
     getHouseResult: () => {
         return new Promise(async (resolve, reject) => {
-            let pukainar = await db.get().collection(collection.HOUSE_RESULT_COLLECTION).find({ houseName: "pukainar" }).toArray()
-            let thongal = await db.get().collection(collection.HOUSE_RESULT_COLLECTION).find({ houseName: "thongal" }).toArray()
-            let haqana = await db.get().collection(collection.HOUSE_RESULT_COLLECTION).find({ houseName: "haqana" }).toArray()
-            let murukkam = await db.get().collection(collection.HOUSE_RESULT_COLLECTION).find({ houseName: "murukkam" }).toArray()
+            let pukainar = db.get().collection(collection.HOUSE_RESULT_COLLECTION).find({ houseName: "pukainar" }).toArray()
+            let thongal =  db.get().collection(collection.HOUSE_RESULT_COLLECTION).find({ houseName: "thongal" }).toArray()
+            let haqana =  db.get().collection(collection.HOUSE_RESULT_COLLECTION).find({ houseName: "haqana" }).toArray()
+            let murukkam =  db.get().collection(collection.HOUSE_RESULT_COLLECTION).find({ houseName: "murukkam" }).toArray()
             
             let houseBaseShow = [
                 pukainar = pukainar[0],
