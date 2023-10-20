c02842df9d8f4c585465cb6d820c570162698a28
Thoufeeque Rahman <78486724+Thoufeeque-Rahman@users.noreply.github.com>
2023-10-20 20:36:54 +0530

    await

--- api/index.js
+++ api/index.js
@@ -35,7 +35,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(session({secret:"Key"}))
+app.use(session({
+  secret: "Key",
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(fileUpload())
 
 db.connect((err)=>{
